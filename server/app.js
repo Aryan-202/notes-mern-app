a2bb646f9170cb9620c508ff1f505a5a4bb33481
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-10-21 01:14:53 +0530

    functions

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
+// import authRouter from './routers/authRouter.js';
 
 const app = express();
 
-// Middleware
+
 app.use(express.json());
+
+
+// app.use('/auth',authRouter)
+
 
 // Routes
 app.get('/', (req, res) => {
