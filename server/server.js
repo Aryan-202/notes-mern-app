a835b189acd84fb2b9f0ebb8e4ad0ad89c6977f9
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-10-21 00:40:58 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -6,9 +6,13 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/',(req, res)=>{
-    res.status(200).send('hello')
-})
+    res.status(200).send('hello');
+});
+
+app.get('/health',(req, res)=>{
+    res.status(200).send('server is healthy');
+});
 
 app.listen(PORT, ()=>{
-    console.log(`server is running at port ${PORT}`)
-})
+    console.log(`server is running at port ${PORT}`);
+});
