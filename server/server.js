2b3951e58418b74a1a2228d0111149a059f0d9c9
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-10-21 00:30:02 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -1,3 +1,14 @@
 import express from 'express';
+import { config } from 'dotenv';
 
-app = express()
+config()
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.get('/',(req, res)=>{
+    res.status(200).send('hello')
+})
+
+app.listen(PORT, ()=>{
+    console.log(`server is running at port ${PORT}`)
+})
