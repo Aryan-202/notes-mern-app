8c5d88d547331bffd608eecb0cbf618950d05a02
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-10-21 21:12:26 +0530

    codes for logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './components/home/Home'
+import Login from './components/auth/login/Login';
+import Register from './components/auth/register/Register';
+
 
 
 function App() {
@@ -10,6 +13,8 @@ function App() {
       <Router>
         <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
         </Routes>
       </Router>
     </>
--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Login from '../auth/login/Login'
+import Register from '../auth/register/Register'
 
 const Home = () => {
+
+    const navigate = useNavigate()
+
   return (
-    <div>Home</div>
+    <div>
+        
+        <button onClick={navigate('/login')}>
+            login
+        </button>
+        <button onClick={navigate('/register')}>
+            register
+        </button>
+
+
+    </div>
   )
 }
 
