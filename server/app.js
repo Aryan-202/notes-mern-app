0c22e70210eb81bc20f730b2def7f1f3ca8b4f62
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-10-21 15:36:43 +0530

    user login working

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-// import authRouter from './routers/authRouter.js';
+import authRouter from './routers/authRouter.js';
 
 const app = express();
 
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 
-// app.use('/auth',authRouter)
+app.use('/auth',authRouter)
 
 
 
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import userModel from "../models/user.model.js";
 import bcrypt from 'bcrypt';
 
 
+
 export const register = async (req, res)=>{
     const {name, email, password} = req.body
 
@@ -31,11 +32,56 @@ export const register = async (req, res)=>{
         await user.save()
 
         
-
+        res.status(201).json({
+            success: true,
+            message: 'user created'
+        })
 
 
     } catch (error) {
-        
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export const login = async  (req, res)=>{
+    const {email, password} = req.body;
+    if(!email || !password){
+        return res.status(400).json({
+            success: false,
+            message: 'provide credentials'
+        });
     }
 
+    try {
+        const user = await userModel.findOne({email});
+        if(!user){
+            return res.status(400).json({
+                success: false,
+                message: 'user not present'
+            });
+        };
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordValid){
+            return res.status(400).json({
+                success: false,
+                message: 'wrong password'
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: 'logged in successfully'
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
 }
