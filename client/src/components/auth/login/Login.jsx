099f9686bde7484fb8bc6fa533cc92c1a672b2fd
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-10-21 21:47:26 +0530

    basic css

--- client/src/components/auth/login/Login.jsx
+++ client/src/components/auth/login/Login.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import style from './login.module.css'
 
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+
+      <form action="" className={style.centre}>
+        <label htmlFor="">email</label>
+        <input type="email" name="" id="" />
+        <br />
+        <label htmlFor="">password</label>
+        <input type="password" name="" id="" />
+      </form>
+
+    </div>
   )
 }
 
--- client/src/components/auth/register/Register.jsx
+++ client/src/components/auth/register/Register.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import style from './register.module.css'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div>
+      <form action="" className={style.centre}>
+        <label htmlFor="">name</label>
+        <input type="text" name="" id="" />
+        <br />
+        <label htmlFor="">email</label>
+        <input type="email" name="" id="" />
+        <br />
+        <label htmlFor="">password</label>
+        <input type="password" name="" id="" />
+      </form>
+      <button type="submit" className={style.button}>
+        register
+      </button>
+    </div>
   )
 }
 
